f2952c37cfa294955904fa209d07fbeb5da06d05
sanyokerem <sanyokerem@gmail.com>
2023-08-01 14:56:00 +0300

    RB-1732 : deprecations fixed

--- Command/BackupCommand.php
+++ Command/BackupCommand.php
@@ -41,26 +41,27 @@ An example of usage of the command:
 <info>./app/console database:backup "my-connection-service-id" "/var/tmp" [my_sql_file_name.sql]</info>
 
 EOT
-            )
-        ;
+            );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $container = $this->getContainer();
-        
+
         $factory = $container->get('backup_restore.factory');
         $connectionServiceId = $input->getArgument('connection-service-id');
         $directory = $input->getArgument('target-dir');
         $fileName = $input->getArgument('filename') ? $input->getArgument('filename') : null;
-        
+
         $backupInstance = $factory->getBackupInstance($connectionServiceId);
-        
+
         $backupPath = $backupInstance->backupDatabase($directory, $fileName);
-        
+
         $connection = $container->get($connectionServiceId);
-        
+
         $output->writeln(sprintf('<comment>></comment> <info>Backup was successfully created in "%s".</info>', $backupPath));
+
+        return self::SUCCESS;
     }
 
     public function getContainer(): ContainerInterface
--- Restore/MySql/MySqlRestore.php
+++ Restore/MySql/MySqlRestore.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ENC\Bundle\BackupRestoreBundle\Restore\MySql;
 
 use ENC\Bundle\BackupRestoreBundle\Restore\AbstractRestore;
@@ -6,7 +7,16 @@ use ENC\Bundle\BackupRestoreBundle\Exception\RestoreException;
 
 class MySqlRestore extends AbstractRestore
 {
-    public function restoreDatabase($file)
+    const PARAM_KEY_HOST = 'host';
+    const PARAM_KEY_PORT = 'port';
+    const PARAM_KEY_USER = 'user';
+    const PARAM_KEY_PASSWORD = 'password';
+    const PARAM_KEY_DBNAME = 'dbname';
+
+    /**
+     * @throws RestoreException
+     */
+    public function restoreDatabase($file): void
     {
         if (!is_string($file)) {
             throw new \InvalidArgumentException('First argument must be a string with the full path to the SQL file.');
@@ -15,37 +25,42 @@ class MySqlRestore extends AbstractRestore
         if (!is_file($file)) {
             throw new \InvalidArgumentException(sprintf('File "%s" does not exist.', $file));
         }
-        
+
         $this->callVendorRestoreTool($file);
     }
-    
-    public function callVendorRestoreTool($file)
+
+    /**
+     * @throws RestoreException
+     */
+    public function callVendorRestoreTool($file): void
     {
         if (!$this->doCallVendorRestoreTool($file)) {
             $exception = new RestoreException('An error occurred while working on the restore of your database. For more details, please look at the output of the command using the "getOutput" method of the exception.');
             $exception->setOutput($this->getLastCommandOutput());
-            
+
             throw $exception;
         }
     }
-    
-    protected function doCallVendorRestoreTool($file)
+
+    protected function doCallVendorRestoreTool($file): bool
     {
-        $connection = $this->getConnection();
+        $params = $this->getConnection()->getParams();
         $returnValue = '';
         $output = array();
-        $commandToExecute = sprintf('mysql --host="%s" --port="%s" --user="%s" --password="%s" %s < %s;', 
-            $connection->getHost(), 
-            $connection->getPort(), 
-            $connection->getUsername(), 
-            $connection->getPassword(), 
-            $connection->getDatabase(),
-            $file);
-            
+        $commandToExecute = sprintf(
+            'mysql --host="%s" --port="%s" --user="%s" --password="%s" %s < %s 2>&1;',
+            $params[static::PARAM_KEY_HOST],
+            $params[static::PARAM_KEY_PORT],
+            $params[static::PARAM_KEY_USER],
+            $params[static::PARAM_KEY_PASSWORD],
+            $params[static::PARAM_KEY_DBNAME],
+            $file
+        );
+
         $returnLine = exec($commandToExecute, $output, $returnValue);
-        
+
         $this->setLastCommandOutput($output);
-        
+
         if ($returnValue !== 0) {
             return false;
         } else {
